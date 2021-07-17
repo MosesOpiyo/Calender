c92992973540b6e751b08751e886225f653b915a
Moses <moses.junior@student.moringaschool.com>
2021-07-17 21:07:22 +0300

    addtion of code in all files

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-function sayGreeting(greeting) {
-    var response = prompt("what is your name?");
-    alert(greeting + response);
+function checkBlank() {
+    var message = "";
+    
+    if(document.getElementById('day').value == ""){
+        message += "Enter your day of birth \n";
+        document.getElementById('day').style.borderColor = "red";
+   
+    }
+    if(document.getElementById('month').value == ""){
+        message += "Enter your month of birth \n";
+        document.getElementById('month').style.borderColor = "red";
+    }
+    if(document.getElementById('year').value == ""){
+        message += "Enter your year of birth \n";
+        document.getElementById('year').style.borderColor = "red";
+    }
+    if(message !="") {
+        alert(message);
+        return false
+    } 
 }
-sayGreeting("Hello ")
+function checkRadio() {
+
+}
+function checkDate() {
+    let day = document.getElementById('day').value
+    let month = document.getElementById('month').value
+    let year = document.getElementById('year').value
+
+    if(day <= 0, day > 31){
+        alert("day of birth is invalid");
+        document.getElementById('day').style.borderColor = "red";
+
+    }
+    if(month <= 0, month > 12) {
+        alert("month of birth is invalid");
+        document.getElementById('month').style.borderColor = "red";
+    }
+}
