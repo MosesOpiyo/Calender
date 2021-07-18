d166a380d9807da9cda8af1f39fbf2fcd13d9647
Moses <moses.junior@student.moringaschool.com>
2021-07-18 21:48:03 +0300

    adjustments to all files

---
+++ README.md
@@ -0,0 +1,11 @@
+{Akan Output calender}
+## Description
+{This is a webpage thats allows users to enter the birth dates and get an akan name referred from the day of their birth.}
+
+
+## Technologies Used
+{The webpage was created with HTML for structure, CSS for design and JavaScript for Functionality.}
+
+### License
+The licence used is an MIT licence.
+  
--- main.js
+++ main.js
@@ -1,74 +1,91 @@
+const form = document.getElementById("form")
+let userDay = document.getElementById('dayinput').value 
+let userMonth = document.getElementById('monthinput').value 
+let userYear = document.getElementById('yearinput').value 
 function checkBlank() {
     var message = "";
     
-    if(document.getElementById('day').value == ""){
+    if(document.getElementById('dayinput').value == ""){
         message += "Enter your day of birth \n";
-        document.getElementById('day').style.borderColor = "red";
+        document.getElementById('dayinput').style.borderColor = "red";
    
     }
-    if(document.getElementById('month').value == ""){
+    if(document.getElementById('monthinput').value == ""){
         message += "Enter your month of birth \n";
-        document.getElementById('month').style.borderColor = "red";
+        document.getElementById('monthinput').style.borderColor = "red";
     }
-    if(document.getElementById('year').value == ""){
+    if(document.getElementById('yearinput').value == ""){
         message += "Enter your year of birth \n";
-        document.getElementById('year').style.borderColor = "red";
+        document.getElementById('yearinput').style.borderColor = "red";
     }
     if(message !="") {
         alert(message);
         return false
     } 
 }
-function checkDate() {
-    let day = document.getElementById('day').value
-    let month = document.getElementById('month').value
-    let year = document.getElementById('year').value
 
 
-    if(day <= 0, day > 31){
+function correctDay() {
+    if(userDay <= 0, userDay > 31){
         alert("day of birth is invalid");
-        document.getElementById('day').style.borderColor = "red";
-        return false;
-
+        document.getElementById('dayinput').style.borderColor = "red";
+        return false;  
     }
-    if(month <= 0, month > 12) {
-        alert("month of birth is invalid");
-        document.getElementById('month').style.borderColor = "red";
-        return false;
+    else{
+        return true;
     }
-
+    
 }
+
+function correctMonth() {
+    if(userMonth <= 0, userMonth > 12) {
+        alert("month of birth is invalid");
+        document.getElementById('monthinput').style.borderColor = "red";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+var birthDay = parseInt(Math.round(((CC/4)-2*CC-1)+((5*YY/4))+((26*( userMonth +1)/10))+userDay )%7);
+let numb = parseInt(birthDay)
+
 function checkRadio() {
     var Male = document.getElementById("male");
     var Female = document.getElementById("female");
     var nameMl = ["Kwasi","Kwandwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var nameFml = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-    let DD = parseInt(day);
-    let MM = parseInt(month);
     
-    let yr = year.toString()
+    
+    let yr = userYear.toString()
     var d1 = yr[0]
     var d2 = yr[1]
     var d3 = yr[2]
     var d4 = yr[3]
    
-    var CC = parseInt('' + d1 + d2);
-    var YY = parseInt('' + d3 + d4);
+    let CC = parseInt('' + d1 + d2);
+    let YY = parseInt('' + d3 + d4);
+    
+    
 
-    var birthDay = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
-    var theDay = Math.round(birthDay);
+   
+
 
     if(Male.checked == true){
-        alert('you are a male' + nameMl[theDay]);
+        
+    
+        document.getElementById('output').textContent = "Your Akan name is " + nameMl[numb];
+    
+    }
+        
+        
        
-    }
+    
     if(Female.checked == true){
-        alert('you are a female' + nameFml[theDay]);
+        document.getElementById('output').textContent = "Your Akan name is " + nameFml[numb];
+         
     }
-    else{
-        alert('Please check your gender');
-        return false;
-    }
+
     
     
 
