6ec10075d9c5dee802b953a37bfcf626e08d3199
Moses <moses.junior@student.moringaschool.com>
2021-07-21 16:07:10 +0300

    adjustments to html,css and js files

--- main.js
+++ main.js
@@ -1,87 +1,56 @@
-const form = document.getElementById("form")
-let userDay = document.getElementById('dayinput').value 
-let userMonth = document.getElementById('monthinput').value 
-let userYear = document.getElementById('yearinput').value 
-function checkBlank() {
-    var message = "";
-    
-    if(document.getElementById('dayinput').value == ""){
-        message += "Enter your day of birth \n";
-        document.getElementById('dayinput').style.borderColor = "red";
-   
+//Master function
+var masterFunction = () => {
+    // assign values
+    var day = parseInt(document.getElementById("day").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    //date validators
+    if (month > 12 || month < 1) {
+        alert("Please enter a valid month number!")
+    } else if (year >= 2021 && month >= 7 && day > 18) {
+        alert("Please enter a valid date! Prior of today's date.")
+    } else if (day < 1) {
+        alert("Please enter a number of days greater than 1!")
+    } else if (year % 4 === 0 && month === 2 && day > 29) {
+        alert("Please enter valid days for February in a leap year!")
+    } else if (year % 4 !== 0 && month === 2 && day > 28) {
+        alert("Please enter valid days for February in a non-leap year!")
+    } else if ((month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) && day > 31) {
+        alert("Please enter a valid day response!")
+    } else if ((month === 4 || month === 6 || month === 9 || month === 11) && day > 30) {
+        alert("Please enter a valid response!")
+    } else {
+        //Day of week calculator
+        var CC = parseInt(year.toString().slice(0, 2));
+        var YY = parseInt(year.toString().slice(2, 4));
+        var MM = parseInt(month);
+        var DD = parseInt(day);
+        var dayNumber = Math.round((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
+        //Arrays
+        var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        var femaleNames = ["AKosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        //index geneator
+        if (dayNumber === 0) {
+            var index = 6;
+        } else {
+            var index = dayNumber - 1;
+        }
+        // get gender
+        var getGender = () => {
+            if (document.getElementById("male").checked) {
+                gender = "male";
+            } else if (document.getElementById("female").checked) {
+                gender = "female";
+            }
+            var gender = gender;
+            //Final function
+            if (gender === "male") {
+                document.getElementById("heading").innerHTML = ("Hello " + maleNames[index] + " " + document.getElementById("secondname").value + ". You were born on a " + dayNames[index] + ".");
+            } else if (gender === "female") {
+                document.getElementById("heading").innerHTML = "Hello " + femaleNames[index] + " " + document.getElementById("secondname").value + ". You were born on a " + dayNames[index] + ".";
+            }
+        }
+        getGender();
     }
-    if(document.getElementById('monthinput').value == ""){
-        message += "Enter your month of birth \n";
-        document.getElementById('monthinput').style.borderColor = "red";
-    }
-    if(document.getElementById('yearinput').value == ""){
-        message += "Enter your year of birth \n";
-        document.getElementById('yearinput').style.borderColor = "red";
-    }
-    if(message !="") {
-        alert(message);
-        return false
-    } 
 }
-
-
-function correctDay() {
-    if(userDay <= 0, userDay > 31){
-        alert("day of birth is invalid");
-        document.getElementById('dayinput').style.borderColor = "red";
-        return false;  
-    }
-    else{
-        return true;
-    }
-    
-}
-
-function correctMonth() {
-    if(userMonth <= 0, userMonth > 12) {
-        alert("month of birth is invalid");
-        document.getElementById('monthinput').style.borderColor = "red";
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-var birthDay = parseInt(Math.round(((CC/4)-2*CC-1)+((5*YY/4))+((26*( userMonth +1)/10))+userDay )%7);
-
-let yr = userYear.toString()
-    var d1 = yr[0]
-    var d2 = yr[1]
-    var d3 = yr[2]
-    var d4 = yr[3]
-   
-    let CC = parseInt('' + d1 + d2);
-    let YY = parseInt('' + d3 + d4);
-
-function checkRadio() {
-    var Male = document.getElementById("male");
-    var Female = document.getElementById("female");
-    var nameMl = ["Kwasi","Kwandwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var nameFml = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-
-    if(Male.checked == true){
-        let numb = parseInt(birthDay)
-        
-    
-        document.getElementById('output').textContent = "Your Akan name is " + nameMl[numb];
-    
-    }
-        
-        
-       
-    
-    if(Female.checked == true){
-        document.getElementById('output').textContent = "Your Akan name is " + nameFml[numb];
-         
-    }
-
-    
-    
-
-}
-
